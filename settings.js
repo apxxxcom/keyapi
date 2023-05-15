c1900bb47ad3e2531f8039dd6aa729e7f9e63c19
apxxx.com <132904732+apxxxcom@users.noreply.github.com>
2023-05-15 10:01:04 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'Apxxx. '// yourname
 global.MONGO_DB_URI = "mongodb+srv://apxxx:apxxx123@cluster0.yh2drud.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "apxxx.com" //isi apa aja bebas
 global.your_email = "apxxxbusines@gmail.com" //email
-global.email_password = "apx12345678" //application password email
+global.email_password = "apxxx123" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
